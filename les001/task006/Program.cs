d765381b43ef4a183adffe7a5e0f02b9f41d45d9
Kotezko <grimmjow2505@gmail.com>
2022-10-04 17:45:35 +0300

    corrective

--- les001/task006/Program.cs
+++ les001/task006/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Показать четные числа от 1 до N
 
-Console.WriteLine("введите число");
+Console.WriteLine("введите любое число больше 1");
 int a = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("получается такой числовой ряд:");
 int b = 1;
 
-if (a > 0)
+if (a > 1)
 {
     while (b <= a)
     {
