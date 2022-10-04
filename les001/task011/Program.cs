b282fef1ecc04e8543cf0baf11f05d4f09fbbe76
Kotezko <grimmjow2505@gmail.com>
2022-10-04 16:26:43 +0300

    task011 complete??

---
+++ les001/task011/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("ведите число которое нужно поделить");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("ведите число на которое нужно поделить");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+
+if (numA % numB == 0)
+{
+    Console.WriteLine("число " + numA + " кратно числу " + numB);
+}
+else 
+{
+    Console.WriteLine("число " + numA + " не кратно числу " + numB + ", остаток после деления: " + numA % numB);
+}
