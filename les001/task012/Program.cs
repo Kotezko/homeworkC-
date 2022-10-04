ce723055abf3b4db0189497f6bd58ad68171f4d7
Kotezko <grimmjow2505@gmail.com>
2022-10-04 17:32:59 +0300

    task012 complete

---
+++ les001/task012/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("введите число");
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+if (num > 99)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine(num % 10);
+}
+else 
+{
+    Console.WriteLine("в этом числе нету третьей цифры");
+}
