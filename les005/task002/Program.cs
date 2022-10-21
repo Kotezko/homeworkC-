4126a81cdc7298e2bb21d09e95b75549fd801628
Kotezko <grimmjow2505@gmail.com>
2022-10-21 16:36:17 +0300

    les005 complete

---
+++ les005/task002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу масштабирования фигуры
+
+double[] Scaling (string[] array, double k)
+{
+    double[] coord = new double[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        coord[i] = Convert.ToDouble(array[i]) *k;
+    }
+    return coord;
+}
+
+void PrintArray(double []array)
+{
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        Console.Write($"({array[i]}, {array[i+1]})");
+    }
+}
+Console.Write("Введите координаты точек фигуры через запятую ',': ");
+string[] figure = (Console.ReadLine() ?? "0").Split(',');
+Console.Write("Введите коэфициент масштабирования: ");
+double k = double.Parse(Console.ReadLine() ?? "0");
+double[] ScaledFigure = Scaling(figure, k);
+PrintArray(ScaledFigure);
---
+++ les005/task003/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать программу копирования массива
+
+void FillArray(int[] array,int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+     var random = new Random();
+     array[i] = random.Next(min, max);   
+    }
+}
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+int[] CopyArray(int[] array)
+{
+    int[] arr = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i] = array[i];
+    }
+    return arr;
+}
+int[] array = new int[10];
+FillArray(array, -10, 10);
+Console.WriteLine("array:");
+PrintArray(array);
+int[] arr = CopyArray(array);
+Console.WriteLine("arr - copy of array:");
+PrintArray(arr);
