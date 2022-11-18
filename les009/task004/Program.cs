61803719edb1c2f4e1ce93c4d7c001f5db325a34
Kotezko <grimmjow2505@gmail.com>
2022-11-18 15:08:42 +0300

    corrections

--- les009/task004/Program.cs
+++ les009/task004/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
 // Первые два элемента последовательности задаются пользователем
 
-void SummChis(int a, int b, int n, int count)
+void Sum(int a, int b, int n, int count)
 {
     if (n <= count)
     {
         int sum = a + b;
         Console.Write($"{sum} ");
-        SummChis(b,sum,n+1,count);
+        Sum(b,sum,n+1,count);
     }
 }
 
@@ -17,4 +17,4 @@ Console.Write("Введите второе число: ");
 int B = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите количество чисел для показа: ");
 int count = int.Parse(Console.ReadLine() ?? "0");
-SummChis(A, B, 1, count);
+Sum(A, B, 1, count);
